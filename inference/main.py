95d57eb646832018cc3d0a42fc0d94cdfe037c18
asdfry <asdfry@naver.com>
2023-11-19 10:42:32 +0900

    Update: add fp16

--- inference/main.py
+++ inference/main.py
@@ -17,10 +17,16 @@ device_count = torch.cuda.device_count()
 
 @asynccontextmanager
 async def load_model(app: FastAPI):
+    if os.environ["DTYPE"] == "fp16":
+        dytpe = torch.float16
+    else:
+        dytpe = torch.float32
+
     model_path = f"mnt/pretrained-models/{os.environ['MODEL_NAME']}"
     model["image_processor"] = AutoImageProcessor.from_pretrained(model_path)
     model["model"] = AutoModelForImageClassification.from_pretrained(
         model_path,
+        torch_dtype=dytpe,
         trust_remote_code=True,
     ).to(f"cuda:{device_count-1}")
     yield
@@ -38,6 +44,8 @@ def classify_image(file: bytes = File()):
 
     image = Image.open(BytesIO(file))
     inputs = model["image_processor"](image, return_tensors="pt").to(f"cuda:{device_count-1}")
+    if os.environ["DTYPE"] == "fp16":
+        inputs = {key: value.half() for key, value in inputs.items()}
 
     outputs = model["model"](**inputs)
     predicted_label = model["model"].config.id2label[outputs.logits.argmax(-1).item()]
