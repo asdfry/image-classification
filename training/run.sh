751111605b9c1d84ad943beafa18b2fa78203a6c
asdfry <asdfry@naver.com>
2023-11-22 21:33:39 +0900

    Update: add reader

--- training/run.sh
+++ training/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-    echo "Example: $ run.sh 8(number of gpus) node5(node name) gpfs(storage) 4(up scale)"
-else
+if [ $# -ne 5 ]; then
+    echo "Example: $ run.sh 8(number of gpus) node5(node name) gpfs(storage) 4(up scale) gpu(reader mode)"
+
+elif [ $5 == "cpu" ]; then
     rm train val
     ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/val val
+    echo "torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode --dali_cpu ./"
+    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode --dali_cpu ./
+    
+elif [ $5 == "gpu" ]; then
+    rm train val
+    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/val val
+    echo "torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode ./"
     torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode ./
 fi
