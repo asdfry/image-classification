88e6944c5d9203e30b5850a9ceba72dea7b65446
asdfry <asdfry@naver.com>
2023-11-22 22:53:32 +0900

    Update: update image resolution

--- training/run.sh
+++ training/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [ $# -ne 5 ]; then
-    echo "Example: $ run.sh 8(number of gpus) node5(node name) gpfs(storage) 4(up scale) gpu(reader mode)"
+    echo "Example: $ run.sh 8(number of gpus) node5(node name) gpfs(storage) hd(resolution) gpu(reader mode)"
 
 elif [ $5 == "cpu" ]; then
     rm train val
-    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/val val
-    echo "torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode --dali_cpu ./"
-    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode --dali_cpu ./
+    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-$4/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-$4/val val
+    echo "torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-$4 -nc /$3/jsh/volume --workers 4 --fp16_mode --dali_cpu ./"
+    torchrun --nproc_per_node=$1 main.py -b 32 --epochs 5 -c $3-$2-$4 -nc /$3/jsh/volume --workers 4 --fp16_mode --dali_cpu ./
     
 elif [ $5 == "gpu" ]; then
     rm train val
-    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/val val
-    echo "torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode ./"
-    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode ./
+    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-$4/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-$4/val val
+    echo "torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-$4 -nc /$3/jsh/volume --workers 4 --fp16_mode ./"
+    torchrun --nproc_per_node=$1 main.py -b 32 --epochs 5 -c $3-$2-$4 -nc /$3/jsh/volume --workers 4 --fp16_mode ./
 fi
