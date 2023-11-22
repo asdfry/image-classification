53e46e8b5b8def6e3453bfce6d2b55e7473dbc8e
asdfry <asdfry@naver.com>
2023-11-22 17:22:45 +0900

    Add: update run.sh

---
+++ training/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Example: $ run.sh 2(number of gpus) node2(node name) gpfs(storage)"
+else
+    rm train val
+    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy/val val
+    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 3 -c $3-$2 -nc /$3/jsh/volume --workers 4 --fp16_mode -lr mlx5_3 ./
+fi
--- training/run_ontap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm train val && ln -s /ontap/jsh/volume/datasets/imagenet-sample-numpy/train train && ln -s /ontap/jsh/volume/datasets/imagenet-sample-numpy/val val
-torchrun --nproc_per_node=2 main.py -b 1 --epochs 3 -c ontap-node2 -nc /ontap/jsh/volume --workers 4 --fp16_mode -lr mlx5_3 ./
