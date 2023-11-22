46360cda1b7fd1032e0f8139ae72dd7f12dd99b2
asdfry <asdfry@naver.com>
2023-11-22 19:27:39 +0900

    Update: add upscale image

--- training/run.sh
+++ training/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Example: $ run.sh 2(number of gpus) node2(node name) gpfs(storage)"
+if [ $# -ne 4 ]; then
+    echo "Example: $ run.sh 8(number of gpus) node5(node name) gpfs(storage) 4(up scale)"
 else
     rm train val
-    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy/val val
-    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 1 -c $3-$2 -nc /$3/jsh/volume --workers 4 --fp16_mode -lr mlx5_3 ./
+    ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy-up$4/val val
+    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 5 -c $3-$2-up$4 -nc /$3/jsh/volume --workers 4 --fp16_mode ./
 fi
