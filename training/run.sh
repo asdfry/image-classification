865d724c1546159106be2a06ac85b461984a6437
asdfry <asdfry@naver.com>
2023-11-22 18:23:59 +0900

    Update: add upscale image

--- training/run.sh
+++ training/run.sh
@@ -5,5 +5,5 @@ if [ $# -ne 3 ]; then
 else
     rm train val
     ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy/train train && ln -s /$3/jsh/volume/datasets/imagenet-sample-numpy/val val
-    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 3 -c $3-$2 -nc /$3/jsh/volume --workers 4 --fp16_mode -lr mlx5_3 ./
+    torchrun --nproc_per_node=$1 main.py -b 1 --epochs 1 -c $3-$2 -nc /$3/jsh/volume --workers 4 --fp16_mode -lr mlx5_3 ./
 fi
